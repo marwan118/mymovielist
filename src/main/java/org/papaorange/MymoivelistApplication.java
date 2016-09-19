27ca7ce301929ccefdf01ade0826020103cb2526
biantianxiang <biantianxiang@gmail.com>
2016-09-19 22:04:47 +0800

    创建项目文件结构

---
+++ src/main/java/org/papaorange/MymoivelistApplication.java
@@ -0,0 +1,12 @@
+package org.papaorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoivelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoivelistApplication.class, args);
+	}
+}
