beaa1ec3f7d295643169749da74d5a89fb31910b
papaorange <papaorange@icloud.com>
2016-09-21 01:29:04 +0800

    去掉无用import

--- src/main/java/org/papaorange/mymovielist/service/MovieInfoCollector.java
+++ src/main/java/org/papaorange/mymovielist/service/MovieInfoCollector.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.papaorange.mymovielist.model.DoubanMovieInfo;
 import org.papaorange.mymovielist.model.LocalMovieInfo;
