9c9366f61c2306c80849656eb96003c6d5220506
biantianxiang <biantianxiang@gmail.com>
2016-09-22 01:09:58 +0800

    下载图片到本地

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,8 +1,23 @@
+var movies;
+
 $(document).ready(function() {
-
     $.getJSON('/mymovies', function(data) {
-        console.log(data);
-    });
+        movies = data;
 
+        for (var i = 0; i < movies.movieList.length; i++) {
+
+            var span = document.createElement("span");
+            var br = document.createElement("br");
+            var img = document.createElement("img");
+            span.innerHTML = movies.movieList[i].title;
+            img.src = movies.movieList[i].img;
+
+            document.body.appendChild(span);
+            document.body.appendChild(img);
+            document.body.appendChild(br);
+
+        }
+
+    });
 
 });
