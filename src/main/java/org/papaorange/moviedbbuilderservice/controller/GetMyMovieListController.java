252959095e5707ae18cdc250d66370ac08c0df62
papaorange <13995656568@163.com>
2017-01-08 21:47:54 +0800

    更新

--- src/main/java/org/papaorange/moviedbbuilderservice/controller/GetMyMovieListController.java
+++ src/main/java/org/papaorange/moviedbbuilderservice/controller/GetMyMovieListController.java
@@ -1,10 +1,7 @@
 package org.papaorange.moviedbbuilderservice.controller;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-
 import org.papaorange.moviedbbuilderservice.model.MovieDBObject;
 import org.papaorange.moviedbbuilderservice.utils.AppConfigLoader;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,30 +21,8 @@ public class GetMyMovieListController
     @RequestMapping("/mymovies")
     public MovieDBObject getMyMovieList() throws IOException
     {
-
-	String jsonString = "";
-	BufferedReader reader = null;
 	String movieDBPath = AppConfigLoader.getProp("MovieDBOutputPath") + "movieDB.json";
-
-	FileInputStream in = new FileInputStream(movieDBPath);
-
-	try
-	{
-	    InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-	    reader = new BufferedReader(isr);
-	    String line = null;
-	    while ((line = reader.readLine()) != null)
-	    {
-		jsonString += line;
-	    }
-
-	}
-	finally
-	{
-	    reader.close();
-	}
-
-	return JSON.parseObject(jsonString, MovieDBObject.class);
+	return JSON.parseObject(new FileInputStream(movieDBPath), MovieDBObject.class);
     }
 
 }
