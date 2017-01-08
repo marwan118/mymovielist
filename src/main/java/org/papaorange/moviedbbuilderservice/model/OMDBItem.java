96a38f429fbbd4b03b13c9916aadcd06b269ac83
papaorange <13995656568@163.com>
2017-01-08 21:56:30 +0800

    添加omdb item模型

---
+++ src/main/java/org/papaorange/moviedbbuilderservice/model/OMDBItem.java
@@ -0,0 +1,255 @@
+package org.papaorange.moviedbbuilderservice.model;
+
+public class OMDBItem
+{
+    // "Title": "A Man Called Ove",
+    // "Year": "2015",
+    // "Rated": "PG-13",
+    // "Released": "30 Sep 2016",
+    // "Runtime": "116 min",
+    // "Genre": "Comedy, Drama",
+    // "Director": "Hannes Holm",
+    // "Writer": "Fredrik Backman (novel), Hannes Holm",
+    // "Actors": "Rolf Lassgård, Bahar Pars, Zozan Akgün, Tobias Almborg",
+    // "Plot": "59 year old Ove is the block's grumpy man who several years
+    // earlier was deposed as president of the condominium association, but he
+    // could not give a damn about being deposed and therefore keeps looking
+    // over the neighborhood with an iron fist. When pregnant Parvaneh and her
+    // family moves into the terraced house opposite and accidentally backs into
+    // Ove's mailbox it turns out to be an unexpected friendship. A drama comedy
+    // about unexpected friendship, love and the importance of surrounding
+    // yourself with the proper tools.",
+    // "Language": "Swedish, Persian",
+    // "Country": "Sweden",
+    // "Awards": "12 wins & 10 nominations.",
+    // "Poster":
+    // "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE0NDUyOTc2MV5BMl5BanBnXkFtZTgwODk2NzU3OTE@._V1_SX300.jpg",
+    // "Metascore": "69",
+    // "imdbRating": "7.6",
+    // "imdbVotes": "8,620",
+    // "imdbID": "tt4080728",
+    // "Type": "movie",
+    // "Response": "True"
+
+    private String title;
+    private String year;
+    private String rated;
+    private String released;
+    private String runtime;
+    private String genre;
+    private String director;
+    private String writer;
+    private String actors;
+    private String plot;
+    private String language;
+    private String country;
+    private String awards;
+    private String poster;
+    private String metascore;
+    private String imdbRating;
+    private String imdbVotes;
+    private String imdbID;
+    private String type;
+    private String response;
+
+    public String getTitle()
+    {
+	return title;
+    }
+
+    public void setTitle(String title)
+    {
+	this.title = title;
+    }
+
+    public String getYear()
+    {
+	return year;
+    }
+
+    public void setYear(String year)
+    {
+	this.year = year;
+    }
+
+    public String getRated()
+    {
+	return rated;
+    }
+
+    public void setRated(String rated)
+    {
+	this.rated = rated;
+    }
+
+    public String getReleased()
+    {
+	return released;
+    }
+
+    public void setReleased(String released)
+    {
+	this.released = released;
+    }
+
+    public String getRuntime()
+    {
+	return runtime;
+    }
+
+    public void setRuntime(String runtime)
+    {
+	this.runtime = runtime;
+    }
+
+    public String getGenre()
+    {
+	return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+	this.genre = genre;
+    }
+
+    public String getDirector()
+    {
+	return director;
+    }
+
+    public void setDirector(String director)
+    {
+	this.director = director;
+    }
+
+    public String getWriter()
+    {
+	return writer;
+    }
+
+    public void setWriter(String writer)
+    {
+	this.writer = writer;
+    }
+
+    public String getActors()
+    {
+	return actors;
+    }
+
+    public void setActors(String actors)
+    {
+	this.actors = actors;
+    }
+
+    public String getPlot()
+    {
+	return plot;
+    }
+
+    public void setPlot(String plot)
+    {
+	this.plot = plot;
+    }
+
+    public String getLanguage()
+    {
+	return language;
+    }
+
+    public void setLanguage(String language)
+    {
+	this.language = language;
+    }
+
+    public String getCountry()
+    {
+	return country;
+    }
+
+    public void setCountry(String country)
+    {
+	this.country = country;
+    }
+
+    public String getAwards()
+    {
+	return awards;
+    }
+
+    public void setAwards(String awards)
+    {
+	this.awards = awards;
+    }
+
+    public String getPoster()
+    {
+	return poster;
+    }
+
+    public void setPoster(String poster)
+    {
+	this.poster = poster;
+    }
+
+    public String getMetascore()
+    {
+	return metascore;
+    }
+
+    public void setMetascore(String metascore)
+    {
+	this.metascore = metascore;
+    }
+
+    public String getImdbRating()
+    {
+	return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating)
+    {
+	this.imdbRating = imdbRating;
+    }
+
+    public String getImdbVotes()
+    {
+	return imdbVotes;
+    }
+
+    public void setImdbVotes(String imdbVotes)
+    {
+	this.imdbVotes = imdbVotes;
+    }
+
+    public String getImdbID()
+    {
+	return imdbID;
+    }
+
+    public void setImdbID(String imdbID)
+    {
+	this.imdbID = imdbID;
+    }
+
+    public String getType()
+    {
+	return type;
+    }
+
+    public void setType(String type)
+    {
+	this.type = type;
+    }
+
+    public String getResponse()
+    {
+	return response;
+    }
+
+    public void setResponse(String response)
+    {
+	this.response = response;
+    }
+
+}
