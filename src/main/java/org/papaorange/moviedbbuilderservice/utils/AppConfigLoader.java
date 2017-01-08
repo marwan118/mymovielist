c3122ea60d5e96f973ad0ff85742d3b82f577843
Papaorange <13995656568@163.com>
2017-01-09 01:01:52 +0800

    utf-8读取文件

--- src/main/java/org/papaorange/moviedbbuilderservice/utils/AppConfigLoader.java
+++ src/main/java/org/papaorange/moviedbbuilderservice/utils/AppConfigLoader.java
@@ -2,16 +2,19 @@ package org.papaorange.moviedbbuilderservice.utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
-public class AppConfigLoader {
+public class AppConfigLoader
+{
 
-	private static Properties properties = new Properties();
+    private static Properties properties = new Properties();
 
-	public static String getProp(String key) throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("./config/application.properties");
-		properties.load(fileInputStream);
-		fileInputStream.close();
-		return properties.getProperty(key);
-	}
+    public static String getProp(String key) throws IOException
+    {
+	FileInputStream fileInputStream = new FileInputStream("./config/application.properties");
+	properties.load(new InputStreamReader(fileInputStream, "utf-8"));
+	fileInputStream.close();
+	return properties.getProperty(key);
+    }
 }
